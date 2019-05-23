eda9a73c1b4345780aa3e114d3f699f6ebc94415
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-05-23 14:28:00 -0700

    getPost selector test passing

--- src/selectors/postSelectors.test.js
+++ src/selectors/postSelectors.test.js
@@ -1,5 +1,6 @@
 import { getPosts, getPost } from './postSelectors';
 
+
 describe('tests post selectors', () => {
   it('tests getPosts selector', () => {
     const fakeStore = [{
@@ -8,5 +9,11 @@ describe('tests post selectors', () => {
     expect(getPosts(fakeStore)).toEqual([{
       title:'HEY', body:'NO YOU HEY' }, { title:'BYE', body:'NO YOU BYE' }, { title:'OKAY', body:'NO YOU OKAY'
     }]);
+  });
+  it('tests getPost selector', () => {
+    const fakeStore = [{
+      title:'HEY', body:'NO YOU HEY' }, { title:'BYE', body:'NO YOU BYE' }, { title:'OKAY', body:'NO YOU OKAY'
+    }];
+    expect(getPost(fakeStore, 1)).toEqual({ title:'BYE', body:'NO YOU BYE' });
   });
 });
