3be2bb90910b719abaf15f0170a3a12465088600
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-05-28 09:09:44 -0700

    updated tests to match new getPosts with state.props

--- src/selectors/postSelectors.test.js
+++ src/selectors/postSelectors.test.js
@@ -3,9 +3,9 @@ import { getPosts, getPost } from './postSelectors';
 
 describe('tests post selectors', () => {
   it('tests getPosts selector', () => {
-    const fakeStore = [{
-      title:'HEY', body:'NO YOU HEY' }, { title:'BYE', body:'NO YOU BYE' }, { title:'OKAY', body:'NO YOU OKAY'
-    }];
+    const fakeStore = { posts: [{
+      title:'HEY', body:'NO YOU HEY' }, { title:'BYE', body:'NO YOU BYE' }, { title:'OKAY', body:'NO YOU OKAY' }]
+    };
     expect(getPosts(fakeStore)).toEqual([{
       title:'HEY', body:'NO YOU HEY' }, { title:'BYE', body:'NO YOU BYE' }, { title:'OKAY', body:'NO YOU OKAY'
     }]);
